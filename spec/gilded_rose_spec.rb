5314fc06bd2a0df1ca6feb0dcc816d432ec138bb
RSijelmass <R.Sijelmass@gmail.com>
2017-07-12 14:14:20 +0100

    Writes test to check if quality Sulfuras changes - updates method name #update_quality() to #update_quality - writes initial README

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -7,9 +7,17 @@ describe GildedRose do
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      GildedRose.new(items).update_quality
       expect(items[0].name).to eq "foo"
     end
+	end
+
+	describe "Handles Sulfuras" do
+		it "doesn't change the quality of Sulfuras" do
+			items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 5)]		
+			GildedRose.new(items).update_quality
+			expect(items[0].quality).to eq 5
+		end
   end
 
 end
