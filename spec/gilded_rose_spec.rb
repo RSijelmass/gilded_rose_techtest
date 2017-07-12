317febb41c30824a000d526eea891306e1adfe52
RSijelmass <R.Sijelmass@gmail.com>
2017-07-12 15:02:47 +0100

    Writes tests to check for backstage passes - with sell_in ≤ 10 and ≤ 5

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,19 +3,22 @@ require 'test/unit/assertions'
 require 'gilded_rose'
 
 describe GildedRose do
-
+	def create_and_update_item(item, sell_in, quality)
+		items = [Item.new(item, sell_in, quality)]
+		GildedRose.new(items).update_quality	
+		return items;
+	end
+	
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality
+			items = create_and_update_item("foo", 0, 0)
       expect(items[0].name).to eq "foo"
     end
 	end
 
 	describe "handles Sulfuras" do
 		it "doesn't change the quality of Sulfuras" do
-			items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 5)]		
-			GildedRose.new(items).update_quality
+			items = create_and_update_item("Sulfuras, Hand of Ragnaros", 5, 5)
 			expect(items[0].quality).to eq 5
 		end
   end
@@ -30,6 +33,17 @@ describe GildedRose do
 			expect { GildedRose.new(items).update_quality }.to change { items[0].quality }.by 0
 		end
 	end
+
+	describe "handles BackStage Passes" do
+		it "lets the quality rise 2x if sell_in ≤ 10" do
+			items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 8, 0)]
+			expect { GildedRose.new(items).update_quality }.to change { items[0].quality }.by	2
+		end
+		it "lets the quality rise 3x if sell_in ≤ 5" do
+			items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 4, 0)]
+			expect { GildedRose.new(items).update_quality }.to change { items[0].quality }.by	3
+		end
+end
 
 end
 
