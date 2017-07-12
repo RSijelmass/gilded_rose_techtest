9447b2581acfad8e289eb8a4d0a2c79e00ad535b
RSijelmass <R.Sijelmass@gmail.com>
2017-07-12 13:31:46 +0100

    Add failing rspec test to directory - change 'should' statement to 'expect to eq' statement

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,7 +1,16 @@
-=begin
-t_foo
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal items[0].name, "fixme"
+require 'rspec'
+require 'test/unit/assertions'
+require 'gilded_rose'
+
+describe GildedRose do
+
+  describe "#update_quality" do
+    it "does not change the name" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].name).to eq "fixme"
+    end
   end
-=end
+
+end
+
